2e4306e35feb8e7b448d38aaf89f10950a9b2cd1
pallavi-2000 <65910065+pallavi-2000@users.noreply.github.com>
2022-11-15 21:59:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,27 @@
-from collections import namedtuple
-import altair as alt
+pip install pygame
+#2D Graphics library
+
+import pygame
 import math
-import pandas as pd
-import streamlit as st
+pygame.init()
+#initialize all imported pygame modules is a convenient way to get everything started
 
-"""
-# Welcome to Streamlit!
+#first thing setting up a pygame window setting up width & height in pixels
+width,height =800, 800
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+#This function will create a display Surface
+win=pygame.display.set_mode((width,height))
+pygame.display.set_caption("Planet Simulation")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#we will now make a loop
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def main():
+  run=True
 
+  while run:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run=False
+  pygame.quit()
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+main()
